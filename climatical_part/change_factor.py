4ba91f3d375bf681a17f046b611995e4d48a63e4
Gabtup <104354706+Gabtup@users.noreply.github.com>
2025-08-16 11:33:51 +0200

    Update change_factor.py

--- climatical_part/change_factor.py
+++ climatical_part/change_factor.py
@@ -8,18 +8,18 @@ Created on Thu Mar 20 14:38:24 2025
 
 import pandas as pd
 
-# Chargement des fichiers CSV
+# Load CSV files
 file1 = pd.read_csv("/Volumes/evelyn_hv62/rsds_data_raquaypampa/historical/moyennes_mensuelles_1981-2010/moyennes_mensuelles_ACCESS-CM2.csv", sep=",")
 file2 = pd.read_csv("/Volumes/evelyn_hv62/rsds_data_raquaypampa/prediction_ssp126/moyennes_mensuelles_2021-2050/moyennes_mensuelles_ACCESS-CM2.csv", sep=",")
 
-# Conversion des valeurs des moyennes en float pour effectuer les calculs
+# Conversion of mean values to float for calculation purposes.
 file1['Moyenne'] = file1['Moyenne'].astype(float)
 file2['Moyenne'] = file2['Moyenne'].astype(float)
 
-# Calcul du ratio (change factor) pour chaque mois et arrondi à 2 décimales
+# Calculation of the change factor for each month, rounded to two decimal places.
 file1['Change Factor'] = (file2['Moyenne'] / file1['Moyenne']).round(2)
 
-# Sauvegarde du résultat dans un nouveau fichier CSV
+# Saving the result to a new CSV file
 file1[['Mois', 'Change Factor']].to_csv("/Volumes/evelyn_hv62/rsds_data_raquaypampa/stats/change_factor/change_factor_models_new_ssp126/change_factor_ACCESS-CM2.csv", index=False)
 
-print("Le fichier avec les facteurs de changement a été créé avec succès.")
+print("The file containing the change factors was successfully created.")
