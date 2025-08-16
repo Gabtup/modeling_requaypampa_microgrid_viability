9519bb5ae78803cc00ec054277cbe3df46636cf1
Gabtup <104354706+Gabtup@users.noreply.github.com>
2025-08-16 11:27:00 +0200

    Update DIRINT.py

--- RES_part/DIRINT.py
+++ RES_part/DIRINT.py
@@ -10,7 +10,6 @@ import pandas as pd
 import pvlib
 import numpy as np
 
-# === 1. Charger les données ===
 fichier = '/Volumes/evelyn_hv62/part3/data/RES/i/i_global/ssp585/i_2026.csv'
 
 df = pd.read_csv(fichier, parse_dates=['Date'])
@@ -18,14 +17,13 @@ df = df.rename(columns={'Date': 'datetime'})
 df['datetime'] = pd.to_datetime(df['datetime'])
 df = df.set_index('datetime')
 
-# Renommer la colonne GHI
 df = df.rename(columns={'RSDS (W/m²)': 'GHI'})
 
-# === 2. Paramètres du site ===
+# === Site location ===
 latitude = -18.2
 longitude = -64.62
 
-# === 3. Position du soleil ===
+# === Sun position ===
 solar_position = pvlib.solarposition.get_solarposition(
     time=df.index,
     latitude=latitude,
@@ -33,7 +31,7 @@ solar_position = pvlib.solarposition.get_solarposition(
 )
 df['zenith'] = solar_position['zenith']
 
-# === 4. DHI/DNI avec modèle DIRINT ===
+# === find DHI/DNI with DIRINT model ===
 dni = pvlib.irradiance.dirint(
     ghi=df['GHI'],
     solar_zenith=df['zenith'],
@@ -45,21 +43,20 @@ dni = pvlib.irradiance.dirint(
     max_zenith=87
 )
 
-# Remplacer les NaN éventuels par 0 (par exemple avant le lever du soleil)
 dni = dni.fillna(0)
 df['DNI'] = dni
 
-# Calculer DHI à partir de : GHI = DHI + DNI * cos(zenith)
+# Compute DHI from : GHI = DHI + DNI * cos(zenith)
 df['DHI'] = df['GHI'] - df['DNI'] * np.cos(np.radians(df['zenith']))
-df['DHI'] = df['DHI'].clip(lower=0)  # éviter valeurs négatives
+df['DHI'] = df['DHI'].clip(lower=0)  # avoid negative values
 
-# === 5. Irradiation diffuse annuelle ===
+# === Annual irradiation ===
 energie_diffuse_Whm2 = df['DHI'].sum()
 energie_diffuse_kWhm2 = energie_diffuse_Whm2 / 1000
 
-# === 6. Affichage + Export ===
+# === 6. Print + Export ===
 print(f"Irradiation diffuse annuelle : {energie_diffuse_kWhm2:.2f} kWh/m²")
 print(df.head(24))
 
 # Export
-df.round(2).to_csv('/Volumes/evelyn_hv62/part3/data/RES/i/i_isotropic/ssp585/i_2026.csv')
+df.round(2).to_csv('/Volumes/evelyn_hv62/part3/data/RES/i/i_isotropic/ssp585/i_max.csv')
